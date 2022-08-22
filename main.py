1c105db0deda7d8410a6d94964e7c55b77b200f3
helenemartin <helenemartin13@hotmail.com>
2022-08-22 17:23:35 +0100

    allows fastapi CORS middleware to allow connection with fastapi swagger

--- main.py
+++ main.py
@@ -3,15 +3,34 @@ from sqlalchemy.sql.functions import user
 from db import models
 from db.database import engine
 from routers import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(user.router)
 
+
 @app.get("/")
+
 
 def root():
   return "Hello me!"
+
+
+origins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002'
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
+
 
 
 models.Base.metadata.create_all(engine)
